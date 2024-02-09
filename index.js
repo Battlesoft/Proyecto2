15e66893144e3961a3d785288c4ec16fe7681a53
Canario <yeraysantanac@gmail.com>
2024-02-09 18:35:15 +0000

    inicio

--- index.js
+++ index.js
@@ -19,6 +19,19 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// ---------------
+// MY CODE
+// ---------------
+
+app.get('/api/whoami', function (req, res) {
+  const ipaddress = req.ip;
+  res.json({ipaddress});
+});
+
+
+
+// ---------------------------------------
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
